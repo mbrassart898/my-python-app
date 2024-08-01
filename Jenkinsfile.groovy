2873b0f5c1a60e07fdcf6c7b3135680366172b0d
Michel Brassart <MikeBrassart@gmail.com>
2024-08-01 02:43:09 +0200

    Initial commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,14 @@
 pipeline {
     agent any
 
+    environment {
+        PYTHON_ENV = 'venv'  // Virtual environment name
+        AZURE_CREDENTIALS = credentials('jenkins-service-principal') // Jenkins credentials ID for Azure Service Principal
+        AZURE_APP_NAME = 'app.py'  // Name of your Azure Web App
+        AZURE_RESOURCE_GROUP = 'jenkins_test'  // Name of your Azure Resource Group
+        AZURE_LOCATION = 'East US'  // Location of your Azure resources
+    }
+
     stages {
         stage('Checkout') {
             steps {
@@ -12,7 +20,7 @@ pipeline {
             steps {
                 bat '''
                     python -m venv venv
-                    venv\\Scripts\\activate
+                    call venv\\Scripts\\activate
                     pip install -r requirements.txt
                 '''
             }
@@ -21,7 +29,7 @@ pipeline {
         stage('Test') {
             steps {
                 bat '''
-                    venv\\Scripts\\activate
+                    call venv\\Scripts\\activate
                     pytest tests/
                 '''
             }
@@ -30,6 +38,22 @@ pipeline {
         stage('Dummy Step') {
             steps {
                 echo 'This is a dummy step to ensure pipeline runs'
+            }
+        }
+
+        stage('Deploy') {
+            when {
+                branch 'master'
+                expression {
+                    return currentBuild.result == null || currentBuild.result == 'SUCCESS'
+                }
+            }
+            steps {
+                bat '''
+                    az login --service-principal -u ${AZURE_CREDENTIALS_USR} -p ${AZURE_CREDENTIALS_PSW} --tenant ${AZURE_CREDENTIALS_TEN}
+                    call venv\\Scripts\\activate
+                    ./deploy.sh
+                '''
             }
         }
     }
