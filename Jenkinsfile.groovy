27e0c64d11a82c7842da5b2396309fb4559dd7ef
Michel Brassart <MikeBrassart@gmail.com>
2024-08-01 02:36:03 +0200

    Initial commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,6 @@
 pipeline {
     agent any
 
-    environment {
-        PYTHON_ENV = 'venv'  // Virtual environment name
-        AZURE_CREDENTIALS = credentials('jenkins-service-principal') // Jenkins credentials ID for Azure Service Principal
-        AZURE_APP_NAME = 'app.py'  // Name of your Azure Web App
-        AZURE_RESOURCE_GROUP = 'jenkins_test'  // Name of your Azure Resource Group
-        AZURE_LOCATION = 'East US'  // Location of your Azure resources
-    }
-
     stages {
         stage('Checkout') {
             steps {
@@ -16,42 +8,9 @@ pipeline {
             }
         }
 
-        stage('Setup') {
+        stage('Dummy Step') {
             steps {
-                sh '''
-                    python3 -m venv ${PYTHON_ENV}
-                    source ${PYTHON_ENV}/bin/activate
-                    pip install -r requirements.txt
-                '''
-            }
-        }
-
-        stage('Test') {
-            steps {
-                sh '''
-                    source ${PYTHON_ENV}/bin/activate
-                    pytest tests/
-                '''
-            }
-        }
-
-        stage('Deploy') {
-            when {
-                branch 'master'
-                expression {
-                    return currentBuild.result == null || currentBuild.result == 'SUCCESS'
-                }
-            }
-            steps {
-                script {
-                    sh '''
-                        az login --service-principal -u ${AZURE_CREDENTIALS_USR} -p ${AZURE_CREDENTIALS_PSW} --tenant ${AZURE_CREDENTIALS_TEN}
-                    '''
-                    sh '''
-                        source ${PYTHON_ENV}/bin/activate
-                        ./deploy.sh
-                    '''
-                }
+                sh 'echo "This is a dummy step to ensure pipeline runs"'
             }
         }
     }
