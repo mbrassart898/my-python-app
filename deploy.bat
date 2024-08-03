fd61bc27bfaa52b2c1d59325056f6c3d9fcdb781
Michel Brassart <MikeBrassart@gmail.com>
2024-08-04 00:58:37 +0200

    Clened jenkins file

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,17 @@
 @echo off
+setlocal
+
+echo Checking if the web app exists...
+az webapp show --name your-azure-app-url --resource-group jenkins_test >nul 2>&1
+
+if %ERRORLEVEL% NEQ 0 (
+    echo The web app 'your-azure-app-url' does not exist. Creating the web app...
+    az webapp create --name your-azure-app-url --resource-group jenkins_test --plan myAppServicePlan
+    if %ERRORLEVEL% NEQ 0 (
+        echo Failed to create the web app. Exiting.
+        exit /b %ERRORLEVEL%
+    )
+)
 
 echo Deploying to Azure Web App...
 az webapp up --name your-azure-app-url --runtime "PYTHON|3.9"
@@ -10,3 +23,6 @@ if %ERRORLEVEL% NEQ 0 (
     echo Deployment complete.
     exit /b 0
 )
+
+endlocal
+
