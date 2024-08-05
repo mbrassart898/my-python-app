e7a36106705b008a1d2f79ae7655a2ca7ce9a6c5
Michel Brassart <MikeBrassart@gmail.com>
2024-08-05 11:15:41 +0200

    simplified bat file to just deploy update to code

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,7 @@ $RESOURCE_GROUP = "mbcicd"
 $PLAN_NAME = "mb-service-plan"
 $WEBAPP_NAME = "mb-app"
 $LOCATION = "South Central US"
-$RUNTIME = "PYTHON:3.11"
+$RUNTIME = "PYTHON|3.11"
 $OS_TYPE = "linux"
 
 Write-Output "Resource group: $RESOURCE_GROUP"
@@ -13,6 +13,15 @@ Write-Output "Webapp name: $WEBAPP_NAME"
 Write-Output "Location: $LOCATION"
 Write-Output "Runtime: $RUNTIME"
 Write-Output "OS type: $OS_TYPE"
+
+# Create the resource group if it doesn't exist
+az group create --name $RESOURCE_GROUP --location "$LOCATION"
+
+# Create the app service plan if it doesn't exist
+az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --location "$LOCATION" --sku B1 --is-linux
+
+# Create the web app if it doesn't exist
+az webapp create --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --plan $PLAN_NAME --runtime $RUNTIME
 
 Write-Output "Deploying the web app..."
 az webapp up --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --runtime $RUNTIME --os-type $OS_TYPE
