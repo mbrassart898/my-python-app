8c759919a4618aff080bb7e885e8166f19791514
Michel Brassart <MikeBrassart@gmail.com>
2024-08-05 13:39:42 +0200

    simplified bat file to just deploy update to code

--- deploy.ps1
+++ deploy.ps1
@@ -1,60 +1,61 @@
-Write-Output "Starting deployment script..."
+# Define variables
+$RESOURCE_GROUP = 'mbcicd'
+$PLAN_NAME = 'mb-service-plan'
+$WEBAPP_NAME = 'mb-app'
+$LOCATION = 'South Central US'
+$RUNTIME = 'PYTHON:3.11'
 
-$RESOURCE_GROUP = "mbcicd"
-$PLAN_NAME = "mb-service-plan"
-$WEBAPP_NAME = "mb-app"
-$LOCATION = "South Central US"
-$RUNTIME = "PYTHON:3.11"
-$OS_TYPE = "linux"
-
-Write-Output "Resource group: $RESOURCE_GROUP"
-Write-Output "Plan name: $PLAN_NAME"
-Write-Output "Webapp name: $WEBAPP_NAME"
-Write-Output "Location: $LOCATION"
-Write-Output "Runtime: $RUNTIME"
-Write-Output "OS type: $OS_TYPE"
-
-# Check if the resource group exist
-$resourceGroupExists = (az group exists --name 'mbcicd') -eq 'true'
-if ($resourceGroupExists) {
-    Write-Output "*** Resource group exists."
-} else {
+# Check if the resource group exists
+try {
+    $resourceGroup = az group show --name $RESOURCE_GROUP --query "name" -o tsv
+    if ($resourceGroup) {
+        Write-Output "*** Resource group exists."
+    } else {
+        Write-Output "*** Resource group does not exist."
+        # Create the resource group if it doesn't exist
+        az group create --name $RESOURCE_GROUP --location $LOCATION
+    }
+} catch {
     Write-Output "*** Resource group does not exist."
-    # Creating the resource group
-    #az group create --name $RESOURCE_GROUP --location "$LOCATION"
+    # Create the resource group if it doesn't exist
+    az group create --name $RESOURCE_GROUP --location $LOCATION
 }
 
-# Check if the service plan exist
-$servicePlanExists = (az appservice plan show --name 'mb-service-plan' --resource-group 'mbcicd' --query "name" -o tsv) -ne ''
-if ($servicePlanExists) {
-    Write-Output "*** Service plan exists."
-} else {
+# Check if the service plan exists
+try {
+    $servicePlanName = az appservice plan show --name $PLAN_NAME --resource-group $RESOURCE_GROUP --query "name" -o tsv
+    if ($servicePlanName) {
+        Write-Output "*** Service plan exists."
+    } else {
+        Write-Output "*** Service plan does not exist."
+        # Create the app service plan if it doesn't exist
+        az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --location "$LOCATION" --sku B1 --is-linux
+    }
+} catch {
     Write-Output "*** Service plan does not exist."
     # Create the app service plan if it doesn't exist
-    # az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --location "$LOCATION" --sku B1 --is-linux
+    az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --location "$LOCATION" --sku B1 --is-linux
 }
 
-
-# Check if the web app exist
+# Check if the web app exists
 try {
-    $webAppName = az webapp show --name 'mb-app' --resource-group 'mbcicd' --query "name" -o tsv
+    $webAppName = az webapp show --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --query "name" -o tsv
     if ($webAppName) {
         Write-Output "*** Web app exists."
     } else {
         Write-Output "*** Web app does not exist."
         # Create the web app if it doesn't exist
-        az webapp create --name 'mb-app' --resource-group 'mbcicd' --plan 'mb-service-plan' --location "$LOCATION" --runtime 'PYTHON:3.11' 
+        az webapp create --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --plan $PLAN_NAME --runtime $RUNTIME --location $LOCATION
     }
 } catch {
     Write-Output "*** Web app does not exist."
     # Create the web app if it doesn't exist
-    az webapp create --name 'mb-app' --resource-group 'mbcicd' --plan 'mb-service-plan' --runtime 'PYTHON:3.11' 
-    Write-Output "*** Created Web app."
+    az webapp create --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --plan $PLAN_NAME --runtime $RUNTIME --location $LOCATION
 }
 
+# Deploy the app
 Write-Output "Deploying the web app..."
-az webapp up --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --location "$LOCATION" --runtime $RUNTIME --os-type $OS_TYPE
-
+az webapp up --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --runtime $RUNTIME
 if ($LASTEXITCODE -ne 0) {
     Write-Output "Deployment failed with exit code $LASTEXITCODE."
     exit $LASTEXITCODE
