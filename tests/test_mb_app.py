63e0f6ce136345e7279700015ac9fc8d64fea8ce
Michel Brassart <MikeBrassart@gmail.com>
2024-08-05 18:05:09 +0200

    =Major clean-up

--- tests/test_mb_app.py
+++ tests/test_mb_app.py
@@ -1,7 +1,7 @@
 import pytest
 
 # Add the parent directory to the sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from mb_app import add
 
