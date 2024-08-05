fcfebbee78d5433048145b148d88cfbe5428ae0c
Michel Brassart <MikeBrassart@gmail.com>
2024-08-05 13:07:24 +0200

    simplified bat file to just deploy update to code

--- deploy.ps1
+++ deploy.ps1
@@ -48,7 +48,8 @@ try {
 } catch {
     Write-Output "*** Web app does not exist."
     # Create the web app if it doesn't exist
-    # az webapp create --name 'mb-app' --resource-group 'mbcicd' --plan 'mb-service-plan' --runtime 'PYTHON|3.11'
+    az webapp create --name 'mb-app' --resource-group 'mbcicd' --plan 'mb-service-plan' --runtime 'PYTHON:3.11' --location $LOCATION
+    Write-Output "*** Created Web app."
 }
 
 Write-Output "Deploying the web app..."
