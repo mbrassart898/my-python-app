dc23e96ca6e112fb34aa0c85d8be04c71024154e
Michel Brassart <MikeBrassart@gmail.com>
2024-08-05 17:54:50 +0200

    =Major clean-up

--- diagnostic.bat
+++
@@ -1,7 +0,0 @@
-echo Checking PATH
-echo %PATH%
-where cmd
-where python
-where pip
-powershell -Command "Write-Output PowerShell command is working!"
-pause
--- mb_app.py
+++ mb_app.py
@@ -1,16 +1,2 @@
-# mb-app.py
-
-def main():
-    print("Starting the application...")
-
-    # Your application logic here
-    print("Running the application...")
-
-    # Simulate some computation or process
-    result = 42
-    print(f"Computation result: {result}")
-
-    print("Ending the application.")
-
-if __name__ == "__main__":
-    main()
+def add(a, b):
+    return a + b
